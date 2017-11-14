0433e418ee38df24f796c15514d66f3645e5a5bc
Josep Manel Andrés Moscardó <jandres@imim.es>
2017-11-14 17:02:35 +0100

    PR1106 sudo is not necessary anymore

--- userconf.sh
+++ userconf.sh
@@ -51,8 +51,6 @@ echo export C9_DIR=/home/$USER/.c9 >>/home/$USER/c9.env
 echo export PATH="/home/$USER/.c9/node/bin/:/home/$USER/.c9/node_modules/.bin:$PATH" >>/home/$USER/c9.env
 echo export DOCKER_HOST=dind:2375 >>/home/$USER/c9.env
 
-echo "$USER ALL= (root) NOPASSWD: /usr/local/bin/singularity" >>/etc/sudoers
-
 /usr/sbin/sshd
 munged
 su -l -c "source /home/$USER/c9.env && node /c9/server.js -w /home/$USER --listen 0.0.0.0 -a $USER:$PASSWORD" $USER
